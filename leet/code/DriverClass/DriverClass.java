69565a56a8ad6a9f8cde5bba08558123d6cfb82b
Sravan Kumar Reddy Bontha <bonthasravan@gmail.com>
2023-06-10 10:10:51 +0530

    Practice-Day-3

--- leet/code/DriverClass/DriverClass.java
+++ leet/code/DriverClass/DriverClass.java
@@ -1,11 +1,19 @@
 package leet.code.DriverClass;
 
-import leet.code.Problems.Arrays.MaxSubArray;
-import leet.code.Problems.Arrays.TwoSum;
+import leet.code.Problems.Arrays.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class DriverClass {
+    public static void printArray(int[] nums) {
+        IntStream list = Arrays.stream(nums);
+        list.forEach(i -> System.out.print(i + " "));
+    }
 
     public static void main(String[] args) {
+
 
 //         int[] twoSumArray1 = new int[]{3,2,4};
 //         int[] twoSumArray2 = new int[]{3,3};
@@ -17,23 +25,53 @@ public class DriverClass {
 //        twoSum.optimumTwoSum(twoSumArray3,22);
 //        twoSum.twoSum(twoSumArray4,10);
 
-         int[] nums1 = new int[] {1,2,3,1};
-         int[] nums2 = new int[] {1,2,3,4};
-         int[] nums3 = new int[] {1,1,1,3,3,4,3,2,4,2};
-         int[] nums4 = new int[] {1,1};
-         int[] nums5 = new int[] {2,14,18,22,22};
+//         int[] nums1 = new int[] {1,2,3,1};
+//         int[] nums2 = new int[] {1,2,3,4};
+//         int[] nums3 = new int[] {1,1,1,3,3,4,3,2,4,2};
+//         int[] nums4 = new int[] {1,1};
+//         int[] nums5 = new int[] {2,14,18,22,22};
 //         DuplicateChecker dupChecker = new DuplicateChecker();
 //         System.out.println(dupChecker.checkDuplicates(nums1));
 //         System.out.println(dupChecker.checkDuplicates(nums2));
 //         System.out.println(dupChecker.checkDuplicates(nums3));
 //         System.out.println(dupChecker.checkDuplicates(nums4));
 //         System.out.println(dupChecker.checkDuplicates(nums5));
-         int [] a = {-1, -3, -3, -1, -2, -1, -5, -3,-12};
-         MaxSubArray maxSubArray = new MaxSubArray();
-         System.out.println(maxSubArray.maxSubArray(a));
+//         int [] a = {-1, -3, -3, -1, -2, -1, -5, -3,-12};
+//         MaxSubArray maxSubArray = new MaxSubArray();
+//         System.out.println(maxSubArray.maxSubArray(a));
 //         maxSubArray.maxSubArray(nums2);
 //         maxSubArray.maxSubArray(nums3);
 //         maxSubArray.maxSubArray(nums4);
 //         maxSubArray.maxSubArray(nums5);
+
+//         int[] mergedSortedArray1 = new int[]{1,2,3,0,0,0};
+//         int[] mergedSortedArray2 = new int[]{2,5,6};
+//         int[] mergedSortedArray3 = new int[]{0};
+//         int[] mergedSortedArray4 = new int[]{1};
+//
+//         MergeSortedArray mergedSort = new MergeSortedArray();
+//         mergedSort.merge(mergedSortedArray1,3,mergedSortedArray2,3);
+//         System.out.print("[");
+//         for(int i=0;i<mergedSortedArray1.length;i++){
+//           System.out.print(" "+mergedSortedArray1[i]+" ");
+//         }
+
+//        int[] nums = new int[]{1, 2, 1};
+//        int[] nums2 = new int[]{1,3,2,1};
+//        ArrayConcatenation concatenation = new ArrayConcatenation();
+//        int[] solution = concatenation.getConcatenation(nums);
+//        printArray(solution);
+//                printArray(concatenation.getConcatenation(nums));
+        //        printArray(concatenation.getConcatenation(nums2));
+
+
+        int[] nums = new int[]{0, 2, 1, 5, 3, 4};
+        int[] nums2 = new int[]{5, 0, 1, 2, 3, 4};
+        ArrayFromPermutation permutation = new ArrayFromPermutation();
+        printArray(permutation.buildArray(nums));
+        System.out.println("");
+        printArray(permutation.buildArray(nums2));
+
+
     }
 }
---
+++ leet/code/Problems/Arrays/ArrayConcatenation.java
@@ -0,0 +1,55 @@
+package leet.code.Problems.Arrays;
+
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and
+ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+Example 2:
+
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+*/
+
+public class ArrayConcatenation {
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length*2;i++){
+            ans[i] = nums[i%nums.length];
+        }
+        return ans;
+    }
+
+//    public static void main(String[] args) {
+//        getConcatenation(new int[]{1, 3, 2, 1});
+//    }
+}
+
+
+/*
+Memory Optimized Solution
+*/
+
+//class Solution {
+//    public int[] getConcatenation(int[] nums) {
+//        int[] ans = new int[2*nums.length];
+//        for(int i=0;i<=nums.length-1;i++){
+//            ans[i]=nums[i];
+//            ans[i+nums.length]=nums[i];
+//        }
+//        System.gc();
+//        return ans;
+//    }
+//}
